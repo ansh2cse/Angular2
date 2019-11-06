00f86b41fa90f2b52265fa89c2fc702f3004f31a
Anshul Verma <Anshul.Verma@adl.local>
2019-11-06 18:38:00 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  // template: `<h1>
+  // Hello {{name}}
+  // </h1>`,
+  templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
-  name: string = 'Angular 2! demostration';
-  address: string = 'Gurgaon';
-  phone: number = 98731311;
-  gender: number = 1;  
-  city: string = 'Delhi';  
+  // name: string = 'Angular 2! demostration';
+  // address: string = 'Gurgaon';
+  // phone: number = 98731311;
+  // gender: number = 1;  
+  // city: string = 'Delhi';  
+
+  pageHeader: string = 'Employee Details';
+  imagePath: string = '/gmail/about/static/images/logo-gmail.png';
+
+  firstName: string = 'Tom';
+  lastName: string = 'Hanks';
+  isDisabled: boolean = false;
+  badHtml: string = 'Hello <script>alert("Hacked");</script> World';
+
+  getFullName(): string {
+    return this.firstName + this.lastName;
+  }
+
 }
