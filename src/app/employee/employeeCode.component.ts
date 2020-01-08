9ed04de7c57449852a3b29bbdf4dfedb76aa4951
anshul verma <ansh2cse@gmail.com>
2020-01-08 23:28:31 +0530

    40-Observable retry on error

--- src/app/employee/employeeCode.component.ts
+++ src/app/employee/employeeCode.component.ts
@@ -3,6 +3,11 @@ import { IEmployee } from './employee';
 import { EmployeeService } from './employee.service';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+// Import rxjs retry operator
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/operator/retryWhen';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/scan';
 
 @Component({
     templateUrl: 'app/employee/employeeCode.component.html',
@@ -19,34 +24,55 @@ export class EmployeeCodeComponent implements OnInit {
 
     ngOnInit() {
         let empCode: string = this._activatedRoute.snapshot.params['code'];
-        // this._employeeService.getEmployeesByCode(empCode).subscribe(
-        //     (employeeData) => {
+
+        this._employeeService.getEmployeesByCode(empCode)
+            // Chain the retry operator to retry on error.
+            //.retry()
+            // Retry only 3 times if there is an error
+            //.retry(3)
+            // Retry with a delay of 1000 milliseconds (i.e 1 second)
+            //.retryWhen((err)=>err.delay(1000))
+            // Retry 5 times maximum with a delay of 1 second between each retry attempt
+            .retryWhen((err) => {
+                return err.scan((retryCount) => {
+                    retryCount += 1;
+                    if (retryCount < 6) {
+                        this.statusMessage = 'Retrying ...Attempt#' + retryCount;
+                        return retryCount;
+                    }
+                    else {
+                        throw (err);
+                    }
+                }, 0).delay(1000)
+            })
+            .subscribe(
+                (employeeData) => {
+                    if (employeeData == null) {
+                        this.statusMessage = 'Employee with the specified Employee Code does not exist';
+                    }
+                    else {
+                        this.employee = employeeData;
+                    }
+                },
+                (error) => {
+                    this.statusMessage = 'Problem with the service. Please try again after sometime';
+                    console.error(error);
+                }
+            )
+        // this._employeeService.getEmployeeByCodePromise(empCode)
+        //     .then((employeeData) => {
         //         if (employeeData == null) {
-        //             this.statusMessage = 'Employee with the specified Employee Code does not exist';
+        //             this.statusMessage =
+        //                 'Employee with the specified Employee Code does not exist';
         //         }
         //         else {
         //             this.employee = employeeData;
         //         }
-        //     },
-        //     (error) => {
-        //         this.statusMessage = 'Problem with the service. Please try again after sometime';
+        //     }).catch((error) => {
+        //         this.statusMessage =
+        //             'Problem with the service. Please try again after sometime';
         //         console.error(error);
-        //     }
-        // )
-        this._employeeService.getEmployeeByCodePromise(empCode)
-            .then((employeeData) => {
-                if (employeeData == null) {
-                    this.statusMessage =
-                        'Employee with the specified Employee Code does not exist';
-                }
-                else {
-                    this.employee = employeeData;
-                }
-            }).catch((error) => {
-                this.statusMessage =
-                    'Problem with the service. Please try again after sometime';
-                console.error(error);
-            });
+        //     });
 
     }
     onBackButtonClick() {
